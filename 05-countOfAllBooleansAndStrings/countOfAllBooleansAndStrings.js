0e196ec7bc5d4ca0223ed40911722be890640afd
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:36:33 -0500

    solved and tested problem 05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var boolAndStringCounter = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(typeof arr[i] == "boolean" || typeof arr[i] == "string"){
+      boolAndStringCounter++;
+    }
+  }
+  return boolAndStringCounter;
 }
 
 // Do not edit this line;
