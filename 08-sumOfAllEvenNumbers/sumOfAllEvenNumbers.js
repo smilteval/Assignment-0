3330b31c13cb6b25d3181714d2269e4d2ff58626
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:45:25 -0500

    solved and tested problem 08

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var evenNumCounter = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      evenNumCounter++;
+    }
+  }
+  return evenNumCounter++;
 }
 
 // Do not edit this line;
