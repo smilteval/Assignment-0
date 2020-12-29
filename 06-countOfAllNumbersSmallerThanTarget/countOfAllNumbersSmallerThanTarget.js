bfd0fca672864500902d31e1cbf55a5158947e1e
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:38:55 -0500

    solved and tested problem 06

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var smallerCounter = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] < target){
+      smallerCounter++;
+    }
+  }
+  return smallerCounter;
 }
 
 // Do not edit this line;
