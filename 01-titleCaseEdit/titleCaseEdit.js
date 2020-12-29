6764a80b6cd361bb8a4bbc6cb0e55dbe4892b023
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:16:16 -0500

    solved and tested problem 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  title = title.toLowerCase();
+  title = title.split(" "); //outputs an array of separate words
+  for(var i = 0; i < title.length; i++){ //turns the first character of every word to uppercase
+    title[i]=title[i].charAt(0).toUpperCase() + title[i].slice(1);
+  }
+  return title.join(" ");
 }
 
 // Do not edit this line;
