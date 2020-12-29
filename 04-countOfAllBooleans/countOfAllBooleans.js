4689e8d3522b94c38d6fae637f2c0209983311da
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:31:33 -0500

    solved and tested problem 04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var boolCounter = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(typeof arr[i] === "boolean"){
+      boolCounter++;
+    }
+  }
+  return boolCounter;
 }
 
 // Do not edit this line;
