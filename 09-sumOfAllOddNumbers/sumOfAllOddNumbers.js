17d9dd6c7a22e3c7684faeeea81dc9a4daf239b3
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:47:02 -0500

    solved and tested problem 09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var oddNumCounter = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] % 2 != 0){
+      oddNumCounter++;
+    }
+  }
+  return oddNumCounter++;
 }
 
 // Do not edit this line;
