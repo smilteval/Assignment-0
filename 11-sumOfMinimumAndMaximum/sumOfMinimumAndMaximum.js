8ab139ed3fe2ed6eee3ef18e8c13a233b9270db2
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:54:31 -0500

    solved and tested problem 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  var min = nums[0];
+  var max = nums[0];
+  
+  for(var i = 1; i < nums.length; i++){
+    if(nums[i] < min){
+      min = nums[i];
+    }
+    if(nums[i] > max){
+      max = nums[i];
+    }
+  }
+  return min + max;
 }
 
 // Do not edit this line;
