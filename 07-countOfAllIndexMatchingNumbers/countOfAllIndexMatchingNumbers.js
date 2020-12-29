bf85e5f3d3fd7a32310d97e150f133764f91670a
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:42:13 -0500

    solved and tested problem 07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,12 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var matchCounter = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(i == nums[i]){
+      matchCounter++;
+    }
+  }
+  return matchCounter;
 }
 
 // Do not edit this line;
