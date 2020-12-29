c25e2d43bf22f3e76a012003202b59ef300bfe8f
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-28 20:49:40 -0500

    solved and tested problem 10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var numInRangeCounter = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] >= start && nums[i] <= end){
+      numInRangeCounter++;
+    }
+  }
+  return numInRangeCounter++;
 }
 
 // Do not edit this line;
