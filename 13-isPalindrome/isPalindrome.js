43ff07a38f8f2acee5774a1b7eaffd8b813fbacf
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-30 21:18:44 -0500

    solved and tested problem 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Insert code here;
+  var len = word.length;
+
+  for(var i = 0; i < len/2; i++){
+    if(word[i] != word[len-1-i]){
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // Do not edit this line;
