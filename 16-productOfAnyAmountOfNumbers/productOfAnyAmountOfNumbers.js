f057a537b2bcce16ab89794eee2432d1021941f2
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-30 21:48:38 -0500

    solved and tested problem 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,12 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var product = 1;
+
+  for(var i = 0; i < args.length; i++){
+    product *= args[i];
+  }
+
+  return product;
 }
 
 // Do not edit this line;
