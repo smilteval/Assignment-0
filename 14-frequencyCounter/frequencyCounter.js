9c38bae4fb697881b0dc46bf678b8d61e540c643
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-12-30 21:29:44 -0500

    solved and tested problem 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,18 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var letterfrequency = {};
+
+  for(var i = 0; i < word.length; i++){
+    var char = word.charAt(i);
+    if(letterfrequency[char]){
+      letterfrequency[char]++;
+    }
+    else{
+      letterfrequency[char] = 1;
+    }
+  }
+
+  return letterfrequency;
 }
 
 // Do not edit this line;
