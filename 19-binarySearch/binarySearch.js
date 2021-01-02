6004afe5a36e35de7903cc336f1106bc28a73560
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-01 21:00:55 -0500

    solved and tested problem 19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,23 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    var middle = Math.floor(nums.length/2);
+
+    if (target == nums[middle]){
+      return true;
+    }
+
+    else if(target > nums[middle]){
+      return this.binarySearch(nums.slice(middle+1, nums.length), target);
+    }
+
+    else if(target < nums[middle]){
+      return this.binarySearch(nums.slice(0,middle), target);
+    }
+    
+    else{
+      return false;
+    }
   }
 }
 
