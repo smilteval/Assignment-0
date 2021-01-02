6315f5ec9bcc44235dcf3cbda0a35ddff9d557de
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-01 20:38:57 -0500

    solved and tested problem 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,18 @@
 function pairSum(nums, target) {
   // Insert code here;
+  if(nums.length <= 1){
+    throw "the array doesn't have at least two elements";
+  }
+
+  for(var i = 0; i < nums.length; i++){
+    for(var j = 1; i < nums.length; i++){
+      if (nums[i] + nums[j] == target){
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 // Do not edit this line;
