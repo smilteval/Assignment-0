086747e9bb10e5b61590b28a2ebecf225d446920
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-01 20:29:06 -0500

    solved and tested problem 17

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  
+  var union = new Set();
+
+  for(var i = 0; i < args.length; i++){
+    args[i].forEach(union.add, union);
+  }
+
+  return union;
 }
 
 // Do not edit this line;
